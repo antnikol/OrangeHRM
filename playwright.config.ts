f6d6573a2916ad1556cd92c63843a4c3d398f11a
Anton Kolupaiev <antnikol@gmail.com>
2025-03-09 23:09:14 +0200

    add settings to config and yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,15 +22,29 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }], 
+    ['list'],
+    [
+      'allure-playwright',
+      {
+        excludeArtifacts: ['video.mp4'],
+        resultsDir: "allure-results",
+        detail: true,
+        suiteTitle: true,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  timeout: 60000,
 
   /* Configure projects for major browsers */
   projects: [
