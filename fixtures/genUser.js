2a5d21f718f19b58cc8cf027fce84998e99c27e8
Anton Kolupaiev <antnikol@gmail.com>
2025-03-10 08:00:37 +0200

    delete JS files + add bugReportTC_001 + add parallel testing

--- fixtures/genUser.js
+++ fixtures/genUser.js
@@ -2,15 +2,13 @@ import { faker } from "@faker-js/faker"
 
 export const userTestData = () => {
   return {
-    name: faker.animal.petName(),
-    email: faker.internet.email(),
-    username: faker.person.firstName() + faker.string.alpha(5),
-    password: faker.internet.password(),
+    username: faker.person.firstName() + faker.string.alpha(5) + faker.number.int({ max: 10000 }),
+    password: faker.internet.password() + faker.number.int({ max: 10 }),
     incorrectPassword: faker.internet.password(),
     firstname: faker.person.firstName(),
     middlename: faker.person.middleName(),
     lastname: faker.person.lastName(),
-    id: faker.number.int({ min: 10000, max: 999999 }),
+    id: faker.number.int({ min: 100000, max: 9999999 }),
     adminUsername: 'Admin',
     adminPassword: 'admin123',
   }
